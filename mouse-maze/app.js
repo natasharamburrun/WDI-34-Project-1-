75ea587c943aa00b65cf64f2e59a1c49571feb36
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-05-20 13:18:34 +0100

    add the up key to the project

--- mouse-maze/app.js
+++ mouse-maze/app.js
@@ -14,6 +14,8 @@ $(()=> {
     [9,9,9,9,9,0,9,9,9,9,9]
   ];
 
+  let playerLocation ={};
+
   $('#maze').on('mouseover', 'div', function() {
     $('#cell-address').val(`${$(this).data('x')}-${$(this).data('y')}`);
   });
@@ -21,47 +23,59 @@ $(()=> {
   $.each(grid,(i,row) => {
     $.each(row,(j,cell) => {
       const $element = $('<div />');
-
       if(cell === 0){
         $element.addClass('path');
         //cell 0 is path
       } else if (cell === 1){
         $element.addClass('mouse');
+        playerLocation = {x: i, y: j};
+        grid[playerLocation.x][playerLocation.y] = 0;
         // cell 1 is mouse
       } else if (cell === 9){
-        $element.addClass('blank');
+        $element.addClass('wall');
         // cell 9 is blank
       }
-      $element.data({x: i, y: j});
+      $element.attr('data-x', i);
+      $element.attr('data-y', j);
 
       $element.on('click', function(){
         console.log($(this).data());
       });
       $element.appendTo('#maze');
     });
-  });
 
+  });
   $(document).on('keydown', function(e){
     switch(e.which){
       case 38:
         console.log('up');
+        if (grid[playerLocation.x-1][playerLocation.y] === 0 ||
+          grid[playerLocation.x-1][playerLocation.y] === 9){
+          playerLocation.x-=1;
+          moveDirection();
+        }
         break;
       case 39:
         console.log('right');
+        $.mouse;
         break;
       case 40:
         console.log('down');
+        $.mouse;
         break;
       case 37:
         console.log('left');
+        $.mouse;
         break;
     }
   });
-
-
-
+  function moveDirection(){
+    $('.mouse').removeClass('mouse').addClass('path');
+    $(`div[data-x='${playerLocation.x}'][data-y='${playerLocation.y}']`).removeClass('path').addClass('mouse');
+  }
   //Add up, down, right and left movements
   // move the classes on the divs ie player1 and floor start with [1] to [0]
   // only able to use pathways
-  // function moveClasses of divs (arr, fromIndex, toIndex){
+  // function moveClasses of divs
+
 });
