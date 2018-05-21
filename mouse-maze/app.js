85f8570274c7d88d855828435d5206795e30debe
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-05-21 10:03:44 +0100

    added a feature to make the mouse move over the page to the opposite side of the page

--- mouse-maze/app.js
+++ mouse-maze/app.js
@@ -6,13 +6,17 @@ $(()=> {
     [9,9,0,9,9,0,9,9,0,9,9],
     [9,9,0,9,9,0,9,9,0,9,9],
     [9,9,0,9,9,0,9,9,0,9,9],
+    [9,9,0,9,9,0,9,9,0,9,9],
     [0,0,0,0,0,1,0,0,0,0,0],
+    [9,0,9,0,9,0,9,0,9,0,9],
     [9,0,9,0,9,0,9,0,9,0,9],
     [9,0,9,0,9,0,9,0,9,0,9],
     [9,0,9,0,9,0,9,0,9,0,9],
     [0,0,0,0,0,0,0,0,0,0,0],
     [9,9,9,9,9,0,9,9,9,9,9]
   ];
+  //grid 11 by 13
+  //Player mouse cell 1, path cell 0, walls cell 9
 
   let playerLocation ={};
 
@@ -20,70 +24,74 @@ $(()=> {
     $('#cell-address').val(`${$(this).data('x')}-${$(this).data('y')}`);
   });
 
-  $.each(grid,(i,row) => {
-    $.each(row,(j,cell) => {
-      const $element = $('<div />');
-      if(cell === 0){
-        $element.addClass('path');
-        //cell 0 is path
-      } else if (cell === 1){
-        $element.addClass('mouse');
-        playerLocation = {x: i, y: j};
-        grid[playerLocation.x][playerLocation.y] = 0;
-        // cell 1 is mouse
-      } else if (cell === 9){
-        $element.addClass('wall');
-        // cell 9 is blank
-      }
-      $element.attr('data-x', i);
-      $element.attr('data-y', j);
-
-      $element.on('click', function(){
-        console.log($(this).data());
+  function generateGrid(){
+    $.each(grid,(i,row) => {
+      $.each(row,(j,cell) => {
+        const $element = $('<div />');
+        if(cell === 0){
+          $element.addClass('path');
+        } else if (cell === 1){
+          $element.addClass('mouse');
+          playerLocation = {x: i, y: j};
+          grid[playerLocation.x][playerLocation.y] = 0;
+        } else if (cell === 9){
+          $element.addClass('wall');
+        }
+        $element.attr('data-x', i);
+        $element.attr('data-y', j);
+        $element.appendTo('#maze');
       });
-      $element.appendTo('#maze');
+    //Add up, down, right and left movements
     });
-
-  });
+  }
+  generateGrid();
+  // function movePlayer(){
   $(document).on('keydown', function(e){
     switch(e.which){
       case 38:
-        console.log('up');
-        if (grid[playerLocation.x-1][playerLocation.y] === 0){
+        if (grid[playerLocation.x][playerLocation.y] === 0){
           playerLocation.x-=1;
           moveDirection();
+          if(playerLocation.x < 0){
+            playerLocation.x = 12;
+            $(`div[data-x='${playerLocation.x}'][data-y='${playerLocation.y}']`).removeClass('path').addClass('mouse');
+          }
         }
         break;
       case 39:
-        console.log('right');
         if (grid[playerLocation.x][playerLocation.y+1] === 0){
           playerLocation.y+=1;
+          console.log('Location---->',grid[playerLocation.x][playerLocation.y+1]);
           moveDirection();
         }
         break;
       case 40:
-        console.log('down');
         if (grid[playerLocation.x+1][playerLocation.y] === 0){
           playerLocation.x+=1;
+          console.log('Location---->Down',playerLocation);
           moveDirection();
         }
         break;
       case 37:
-        console.log('left');
         if (grid[playerLocation.x][playerLocation.y-1] === 0){
           playerLocation.y-=1;
+          console.log('Location---->Left',playerLocation);
           moveDirection();
         }
         break;
     }
   });
+  // }
+  //add a function to make the character move only in the pathways
+  //move the classes on the divs (moveClasses) ie player and pathway start with [1] to [0]
   function moveDirection(){
     $('.mouse').removeClass('mouse').addClass('path');
     $(`div[data-x='${playerLocation.x}'][data-y='${playerLocation.y}']`).removeClass('path').addClass('mouse');
   }
-  //Add up, down, right and left movements
-  // move the classes on the divs ie player1 and floor start with [1] to [0]
-  // only able to use pathways
-  // function moveClasses of divs
+  //make mouse pathway extended array [0][10]
+  //collect cheese function
+  //collect cat repelent posion function
+  //add 2 cats for level 1
+  //
 
 });
