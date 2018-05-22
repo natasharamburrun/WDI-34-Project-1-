9baec5843603ac4132e93013de71ef720b332c9b
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-05-22 22:41:26 +0100

    made changes to the cats movement and added additional 2 cats so 3 in total

--- mouse-maze/app.js
+++ mouse-maze/app.js
@@ -5,14 +5,14 @@ $(()=> {
     [2,0,2,0,2,0,2,0,2,0,2,0,2,0,2],
     [9,9,9,0,9,9,9,2,9,9,9,0,9,9,9],
     [9,9,9,2,9,9,9,0,9,9,9,2,9,9,9],
-    [9,9,9,0,9,9,9,2,9,9,9,5,9,9,9],
+    [9,9,9,6,9,9,9,2,9,9,9,5,9,9,9],
     [9,9,9,2,9,9,9,0,9,9,9,2,9,9,9],
     [9,9,9,0,9,9,9,0,9,9,9,0,9,9,9],
     [0,2,0,2,0,2,0,1,0,2,0,2,0,2,0],
     [9,9,2,9,2,9,9,0,9,9,2,9,2,9,9],
     [9,9,0,9,0,9,9,0,9,9,0,9,0,9,9],
     [9,9,2,9,2,9,9,2,9,9,2,9,2,9,9],
-    [9,9,0,9,0,9,9,0,9,9,0,9,0,9,9],
+    [9,9,0,9,0,9,9,7,9,9,0,9,0,9,9],
     [9,9,2,9,2,9,9,2,9,9,2,9,2,9,9],
     [2,0,2,0,2,0,2,0,2,0,2,0,2,0,2],
     [9,9,9,9,9,9,9,2,9,9,9,9,9,9,9]
@@ -23,7 +23,9 @@ $(()=> {
   //computer cat cell 5
 
   let playerMovement = {};
-  let catCellPosition = {};
+  let cat1CellPosition = {};
+  let cat2CellPosition = {};
+  let cat3CellPosition = {};
   const catDirection = ['up','down','left','right'];
   let direction = 'up';
 
@@ -45,9 +47,17 @@ $(()=> {
         } else if (cell === 9){
           $element.addClass('wall');
         } else if (cell === 5){
-          $element.addClass('cats');
-          catCellPosition = {x: i, y: j};
-          grid[catCellPosition.x][catCellPosition.y] = 0;
+          $element.addClass('cat1');
+          cat1CellPosition = {x: i, y: j};
+          grid[cat1CellPosition.x][cat1CellPosition.y] = 0;
+        } else if (cell === 6){
+          $element.addClass('cat2');
+          cat2CellPosition = {x: i, y: j};
+          grid[cat2CellPosition.x][cat2CellPosition.y] = 0;
+        } else if (cell === 7) {
+          $element.addClass('cat3');
+          cat3CellPosition = {x: i, y: j};
+          grid[cat3CellPosition.x][cat3CellPosition.y] = 0;
         } else if(cell === 2){
           $element.addClass('treat path');
         } else if (cell === 3){
@@ -156,69 +166,171 @@ $(()=> {
 
   // let cellX;
   // let cellY;
-
-  function moveCat(newPosition, oldPosition) {
+  function moveCat1(newPosition, oldPosition) {
     // console.log(oldPosition, newPosition);
     // if wall
-    if($(`div[data-x='${newPosition.x}'][data-y='${newPosition.y}']`).hasClass('wall')) {
-      // don't move
-      // console.log('bump');
-      return null;
-    } else {
-      // No wall. We're free to move
-      // remove cat from old square
-      $(`div[data-x='${oldPosition.x}'][data-y='${oldPosition.y}']`).removeClass('cats').addClass('path');
-      // add cat at new position
-      $(`div[data-x='${newPosition.x}'][data-y='${newPosition.y}']`).removeClass('path').addClass('cats');
-      // update cat position
-      return 'moved';
-    }
-
+    // if($(`div[data-x='${newPosition.x}'][data-y='${newPosition.y}']`).hasClass('wall')) {
+    //   // don't move
+    //   // console.log('bump');
+    //   return null;
+    // } else {
+    // No wall. We're free to move
+    // remove cat from old square
+    if($(`div[data-x='${oldPosition.x}'][data-y='${oldPosition.y}']`).removeClass('cat1').addClass('path'));
+    // add cat at new position
+    $(`div[data-x='${newPosition.x}'][data-y='${newPosition.y}']`).removeClass('path').addClass('cat1');
+    // update cat position
+    return 'moved';
   }
 
   window.setInterval(function(){
-    direction = catDirection[Math.floor(Math.random() * catDirection.length)];
+    direction = catDirection[Math.floor(Math.random() * 3)];
     // console.log(direction);
     const newPosition = {};
-    newPosition.x = catCellPosition.x;
-    newPosition.y = catCellPosition.y;
+    newPosition.x = cat1CellPosition.x;
+    newPosition.y = cat1CellPosition.y;
     switch(direction) {
       case 'up':
         newPosition.x -= 1;
         if (newPosition.x < 0) newPosition.x = 14;
-        if (moveCat(newPosition, catCellPosition)) catCellPosition = newPosition;
+        if (moveCat1(newPosition, cat1CellPosition)) cat1CellPosition = newPosition;
         break;
 
       case 'right':
         newPosition.y += 1;
         if (newPosition.y > 14) newPosition.y = 0;
-        if (moveCat(newPosition, catCellPosition)) catCellPosition = newPosition;
+        if (moveCat1(newPosition, cat1CellPosition)) cat1CellPosition = newPosition;
         break;
 
       case 'down':
         newPosition.x += 1;
         if (newPosition.x > 14) newPosition.x = 0;
-        if (moveCat(newPosition, catCellPosition)) catCellPosition = newPosition;
+        if (moveCat1(newPosition, cat1CellPosition)) cat1CellPosition = newPosition;
         break;
 
       case 'left':
         newPosition.y -= 1;
         if (newPosition.y < 0) newPosition.y = 14;
-        if (moveCat(newPosition, catCellPosition)) catCellPosition = newPosition;
+        if (moveCat1(newPosition, cat1CellPosition)) cat1CellPosition = newPosition;
         break;
     }
+  }, 400);
 
-  }, 200);
+  function moveCat2(newPosition, oldPosition) {
+    // console.log(oldPosition, newPosition);
+    // if wall
+    // if($(`div[data-x='${newPosition.x}'][data-y='${newPosition.y}']`).hasClass('wall')) {
+    //   // don't move
+    //   // console.log('bump');
+    //   return null;
+    // } else {
+    // No wall. We're free to move
+    // remove cat from old square
+    if($(`div[data-x='${oldPosition.x}'][data-y='${oldPosition.y}']`).removeClass('cat2').addClass('path'));
+    // add cat at new position
+    $(`div[data-x='${newPosition.x}'][data-y='${newPosition.y}']`).removeClass('path').addClass('cat2');
+    // update cat position
+    return 'moved';
+  }
 
+  window.setInterval(function(){
+    direction = catDirection[Math.floor(Math.random() * 3)];
+    // console.log(direction);
+    const newPosition = {};
+    newPosition.x = cat2CellPosition.x;
+    newPosition.y = cat2CellPosition.y;
+    switch(direction) {
+      case 'up':
+        newPosition.x -= 1;
+        if (newPosition.x < 0) newPosition.x = 14;
+        if (moveCat2(newPosition, cat2CellPosition)) cat2CellPosition = newPosition;
+        break;
 
+      case 'right':
+        newPosition.y += 1;
+        if (newPosition.y > 14) newPosition.y = 0;
+        if (moveCat2(newPosition, cat2CellPosition)) cat2CellPosition = newPosition;
+        break;
+
+      case 'down':
+        newPosition.x += 1;
+        if (newPosition.x > 14) newPosition.x = 0;
+        if (moveCat2(newPosition, cat2CellPosition)) cat2CellPosition = newPosition;
+        break;
+
+      case 'left':
+        newPosition.y -= 1;
+        if (newPosition.y < 0) newPosition.y = 14;
+        if (moveCat2(newPosition, cat2CellPosition)) cat2CellPosition = newPosition;
+        break;
+    }
+  }, 300);
+
+  function moveCat3(newPosition, oldPosition) {
+    // console.log(oldPosition, newPosition);
+    // if wall
+    // if($(`div[data-x='${newPosition.x}'][data-y='${newPosition.y}']`).hasClass('wall')) {
+    //   // don't move
+    //   // console.log('bump');
+    //   return null;
+    // } else {
+    // No wall. We're free to move
+    // remove cat from old square
+    if($(`div[data-x='${oldPosition.x}'][data-y='${oldPosition.y}']`).removeClass('cat3').addClass('path'));
+    // add cat at new position
+    $(`div[data-x='${newPosition.x}'][data-y='${newPosition.y}']`).removeClass('path').addClass('cat3');
+    // update cat position
+    return 'moved';
+  }
+
+  window.setInterval(function(){
+    direction = catDirection[Math.floor(Math.random() * 3)];
+    // console.log(direction);
+    const newPosition = {};
+    newPosition.x = cat3CellPosition.x;
+    newPosition.y = cat3CellPosition.y;
+    switch(direction) {
+      case 'up':
+        newPosition.x -= 1;
+        if (newPosition.x < 0) newPosition.x = 14;
+        if (moveCat3(newPosition, cat3CellPosition)) cat3CellPosition = newPosition;
+        break;
+
+      case 'right':
+        newPosition.y += 1;
+        if (newPosition.y > 14) newPosition.y = 0;
+        if (moveCat3(newPosition, cat3CellPosition)) cat3CellPosition = newPosition;
+        break;
+
+      case 'down':
+        newPosition.x += 1;
+        if (newPosition.x > 14) newPosition.x = 0;
+        if (moveCat3(newPosition, cat3CellPosition)) cat3CellPosition = newPosition;
+        break;
+
+      case 'left':
+        newPosition.y -= 1;
+        if (newPosition.y < 0) newPosition.y = 14;
+        if (moveCat3(newPosition, cat3CellPosition)) cat3CellPosition = newPosition;
+        break;
+    }
+  }, 300);
+  //
+  // if(mouse === cat1 && mouse === cat2) {
+  // lifeCounter --
+  // scoreBoard ---
+  //   ;
+  // }
+
+// for postions - clear interval set to how long
   // });
   //add a function to make the character move only in the pathways
   //move the classes on the divs (moveClasses) ie player and pathway start with [1] to [0]
   //
 
-  // start movement of the cats when a key is pressed ie (up) add event (possibly will include false and true statement which includes if up, right, left is pressed )
-  // move cats same theory as moving mouse (ie up, down, left and right) but with set interval when hit wall will turn direction
-  // set interval and stop cats when collision with a mouse or mouse has collected all cheese treats
-  // cats to randomly patrol the pathway
+  // start movement of the cat1 when a key is pressed ie (up) add event (possibly will include false and true statement which includes if up, right, left is pressed )
+  // move cat1 same theory as moving mouse (ie up, down, left and right) but with set interval when hit wall will turn direction
+  // set interval and stop cat1 when collision with a mouse or mouse has collected all cheese treats
+  // cat1 to randomly patrol the pathway
 
 });
