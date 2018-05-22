8e5782c0e3302a03e3305f5f16044e579ea71004
Natasha Ramburrun <r_natasha100@hotmail.com>
2018-05-22 11:52:09 +0100

    saved changes made - made the mouse function work with some refactoring

--- mouse-maze/app.js
+++ mouse-maze/app.js
@@ -1,30 +1,29 @@
 $(()=> {
 
   const grid = [
-    [9,9,9,9,9,9,9,0,9,9,9,9,9,9,9],
-    [0,0,2,0,0,0,0,0,0,0,0,0,2,0,0],
-    [9,9,9,3,9,9,9,0,9,9,9,3,9,9,9],
+    [9,9,9,9,9,9,9,2,9,9,9,9,9,9,9],
+    [2,0,2,0,2,0,2,0,2,0,2,0,2,0,2],
+    [9,9,9,3,9,9,9,2,9,9,9,3,9,9,9],
+    [9,9,9,2,9,9,9,0,9,9,9,2,9,9,9],
+    [9,9,9,5,9,9,9,2,9,9,9,5,9,9,9],
+    [9,9,9,2,9,9,9,0,9,9,9,2,9,9,9],
     [9,9,9,0,9,9,9,0,9,9,9,0,9,9,9],
-    [9,9,5,0,9,9,9,0,9,9,9,0,5,9,9],
-    [9,9,9,0,9,9,9,0,9,9,9,0,9,9,9],
-    [9,9,9,0,9,9,9,0,9,9,9,0,9,9,9],
-    [0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
+    [0,2,0,2,0,2,0,1,0,2,0,2,0,2,0],
+    [9,9,2,9,2,9,9,0,9,9,2,9,2,9,9],
     [9,9,0,9,0,9,9,0,9,9,0,9,0,9,9],
+    [9,9,2,9,2,9,9,2,9,9,2,9,2,9,9],
     [9,9,0,9,0,9,9,0,9,9,0,9,0,9,9],
-    [9,9,0,9,0,9,9,0,9,9,0,9,0,9,9],
-    [9,9,0,9,0,9,9,0,9,9,0,9,0,9,9],
-    [9,9,0,9,0,9,9,0,9,9,0,9,0,9,9],
-    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [9,9,9,9,9,9,9,0,9,9,9,9,9,9,9]
+    [9,9,2,9,2,9,9,2,9,9,2,9,2,9,9],
+    [2,0,2,0,2,0,2,0,2,0,2,0,2,0,2],
+    [9,9,9,9,9,9,9,2,9,9,9,9,9,9,9]
   ];
-
 
   //grid 14 by 14
   //Player mouse cell 1, path cell 0, walls cell 9
-  //cheese cell 2, repelent 3
+  //cheese cell 2, repellent 3
   //computer cat cell 5
 
-  let playerMovement ={};
+  let playerMovement = {};
 
   $('#maze').on('mouseover', 'div', function() {
     $('#cell-address').val(`${$(this).data('x')}-${$(this).data('y')}`);
@@ -44,41 +43,46 @@ $(()=> {
           $element.addClass('wall');
         } else if (cell === 5){
           $element.addClass('cat');
+          catMovement = {x: i, y: j};
+          grid[catMovement.x][catMovement.y] = 0;
         } else if(cell === 2){
           $element.addClass('treat path');
         } else if (cell === 3){
-          $element.addClass('repelent path');
+          $element.addClass('repellent path');
         }
 
         $element.attr('data-x', i);
         $element.attr('data-y', j);
         $element.appendTo('#maze');
       });
-      //Add up, down, right and left movements
     });
   }
   generateGrid();
 
+  //Add up, down, right and left movements
 
 
-  function movePlayer(){
-    $('.mouse').removeClass('mouse').addClass('path');
-    $(`div[data-x='${playerMovement.x}'][data-y='${playerMovement.y}']`).removeClass('treat').removeClass('repelent').addClass('mouse');
-  }
-  // function movePlayer(){
   $(document).on('keydown', function(e){
     const x = playerMovement.x;
     const y = playerMovement.y;
+
     let checkSquareX = x, checkSquareY = y;
+    let $checkSq;
+
+    function getDiv(x, y) {
+      return $(`div[data-x='${checkSquareX}'][data-y='${checkSquareY}']`);
+    }
+    let $playMove = getDiv(x, y);
 
     switch(e.which){
       case 38://up
         checkSquareX = x - 1;
         if (checkSquareX < 0) checkSquareX = 14;
-        if ($(`div[data-x='${checkSquareX}'][data-y='${checkSquareY}']`).hasClass('path')){
-          playerMovement.x -=1;
-          if($(`div[data-x='${playerMovement.x}'][data-y='${playerMovement.y}']`).hasClass('wall')){
-            playerMovement.x+=1;
+        $checkSq = getDiv(checkSquareX, checkSquareY);
+        if ($checkSq.hasClass('path')){
+          playerMovement.x -= 1;
+          if($playMove.hasClass('wall')){
+            playerMovement.x += 1;
             return null;
           }
           if(playerMovement.x < 0){
@@ -90,10 +94,11 @@ $(()=> {
       case 39://right
         checkSquareY = y + 1;
         if (checkSquareY > 14) checkSquareY = 0;
-        if ($(`div[data-x='${checkSquareX}'][data-y='${checkSquareY}']`).hasClass('path')){
-          playerMovement.y+=1;
-          if($(`div[data-x='${playerMovement.x}'][data-y='${playerMovement.y}']`).hasClass('wall')){
-            playerMovement.y-=1;
+        $checkSq = getDiv(checkSquareX, checkSquareY);
+        if ($checkSq.hasClass('path')){
+          playerMovement.y+= 1;
+          if($playMove.hasClass('wall')){
+            playerMovement.y-= 1;
             return null;
           }
           if(playerMovement.y > 14){
@@ -105,10 +110,11 @@ $(()=> {
       case 40://down
         checkSquareX = x + 1;
         if (checkSquareX > 14) checkSquareX = 0;
-        if ($(`div[data-x='${checkSquareX}'][data-y='${checkSquareY}']`).hasClass('path')){
-          playerMovement.x+=1;
-          if($(`div[data-x='${playerMovement.x}'][data-y='${playerMovement.y}']`).hasClass('wall')){
-            playerMovement.x-=1;
+        $checkSq = getDiv(checkSquareX, checkSquareY);
+        if ($checkSq.hasClass('path')){
+          playerMovement.x+= 1;
+          if($playMove.hasClass('wall')){
+            playerMovement.x-= 1;
             return null;
           }
           if(playerMovement.x > 14){
@@ -120,10 +126,11 @@ $(()=> {
       case 37://left
         checkSquareY = y - 1;
         if (checkSquareY < 0) checkSquareY = 14;
-        if ($(`div[data-x='${checkSquareX}'][data-y='${checkSquareY}']`).hasClass('path')){
-          playerMovement.y-=1;
-          if($(`div[data-x='${playerMovement.x}'][data-y='${playerMovement.y}']`).hasClass('wall')){
-            playerMovement.y+=1;
+        $checkSq = getDiv(checkSquareX, checkSquareY);
+        if ($checkSq.hasClass('path')){
+          playerMovement.y-= 1;
+          if($playMove.hasClass('wall')){
+            playerMovement.y+= 1;
             return null;
           }
           if(playerMovement.y < 0){
@@ -134,20 +141,64 @@ $(()=> {
         break;
     }
   });
+  function movePlayer(){
+    $('.mouse').removeClass('mouse').addClass('path');
+    $(`div[data-x='${playerMovement.x}'][data-y='${playerMovement.y}']`).removeClass('treat').removeClass('repelent').addClass('mouse');
+  }
+  //
+  //   function moveCats(){
+  //     $('.cats').removeClass('cats').addClass('path');
+  //
+  //   }
+  // });
   //add a function to make the character move only in the pathways
   //move the classes on the divs (moveClasses) ie player and pathway start with [1] to [0]
-
-  function moveCats(){
-    $('.cats').removeClass('cats').addClass('path');
-    $(`div[data-x='${playerMovement.x}'][data-y='${playerMovement.y}']`).removeClass('cats').addClass('path');
-  }
-
-
-
-  //make mouse pathway extended array [0][10]
-  //collect cheese function
-  //collect cat repelent posion function
-  //add 2 cats for level 1
   //
 
+  // const catMovement = ['up','down','left','right'];
+  // let direction = catMovement[3];
+  //
+  // //moveCats
+  // setInterval(function(){
+  //   switch(direction) {
+  //     case 'up':
+  //       if($(`div[data-x='${catMovement.x}'][data-y='${catMovement.y}']`).hasClass('cats')){
+  //         $(`div[data-x='${catMovement.x}'][data-y='${catMovement.y}']`).removeClass('cats').addClass('path');
+  //       } else {
+  //         direction = catMovement[Math.floor(Math.random() * catMovement.length)];
+  //         console.log(catMovement);
+  //       }
+  //       break;
+  //   }
+  // }, 1000);
+  //
+  // checkMovement();
+
+
+
+  // set intercal called array with position
+  // switch directiomn case right
+  //
+  // if
+  //
+  // els if maths random * catPosition.length
+
+  //
+  //
+  //
+  // //
+  //
+  // start movement of the cats when a key is pressed ie (up) add event (possibly will include false and true statement which includes if up, right, left is pressed )
+  // move cats same theory as moving mouse (ie up, down, left and right) but with set interval when hit wall will turn direction
+  // set interval and stop cats when collision with a mouse or mouse has collected all cheese treats
+  // cats to randomly patrol the pathway
+  //
+  //   startCat.on('click', ()=> {
+
+//   catDirection = Math.floor(Math.random() * 2)
+// //
+// //   setInterval(() => {
+// //     if(playerMovement ===
+// //   });
+  // }
 });
